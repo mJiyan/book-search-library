202532d99e4588050396f9da4a91c2e489f4e09a
mJiyan <m.jiyan_aslan@hotmail.com>
2023-04-04 11:12:01 +0200

    remove unnecessary only function from test case

--- src/shared/storage/booksStorage.test.ts
+++ src/shared/storage/booksStorage.test.ts
@@ -3,7 +3,7 @@ import { BOOK_SEARCH_RESULT_LOCAL_STORAGE_MOCK } from 'src/mock';
 import { getBookSearchResultFromStorage, setBookSearchResultToStorage } from "./booksStorage"
 
 describe("Book localStorage ", () => {
-    it.only("should set and return app state with using localStorage", () => {
+    it("should set and return app state with using localStorage", () => {
         setBookSearchResultToStorage(BOOK_SEARCH_RESULT_LOCAL_STORAGE_MOCK)
         const result = getBookSearchResultFromStorage()
         expect(result).toEqual(BOOK_SEARCH_RESULT_LOCAL_STORAGE_MOCK)
